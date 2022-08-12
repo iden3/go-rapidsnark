612e704362860700c7dcab8e3b1040981c2cd8a8
Oleksandr Brezhniev <oleksandr.brezhniev@gmail.com>
2022-08-12 19:09:43 +0300

    Fix for ld

--- prover/build_linux_amd64.go
+++ prover/build_linux_amd64.go
@@ -3,5 +3,5 @@
 package prover
 
 // #cgo CFLAGS: -DUSE_VENDORED_RAPIDSNARK
-// #cgo LDFLAGS: ${SRCDIR}/rapidsnark_vendor/librapidsnark-linux-amd64.a ${SRCDIR}/rapidsnark_vendor/libgmp-linux-amd64.a -lstdc++ -fopenmp
+// #cgo LDFLAGS: ${SRCDIR}/rapidsnark_vendor/librapidsnark-linux-amd64.a ${SRCDIR}/rapidsnark_vendor/libgmp-linux-amd64.a -lstdc++ -fopenmp -fpie
 import "C"
